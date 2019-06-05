08aa909087da34f7741fbee9e9074eb511275502
mochell <momme.hell@googlemail.com>
2019-06-05 11:24:06 -0700

    solved conflicts

--- stormrecon.py
+++ stormrecon.py
@@ -1,7 +1,4 @@
 import sys
-<<<<<<< HEAD
-sys.path.append('/Users/laure/Desktop/stage/travail/modules/stormy_forerunners/')
->>>>>>> 8ef2f2625ab3c1fbf467d06cf002aba67f6f8566
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -690,11 +687,7 @@ class Storm(object):
 
         #return mdata
 
-<<<<<<< HEAD
     def cut_data(self, time_in, f_data, data, direction, dt_unit, clevs, directional_filtering=False):
-=======
-    def cut_data(self, time_in, f_data, data, direction, dt_unit, clevs):
->>>>>>> 8ef2f2625ab3c1fbf467d06cf002aba67f6f8566
         import numpy.ma as ma
         self.dt_unit=dt_unit
         self.clevs=clevs
@@ -744,9 +737,7 @@ class Storm(object):
         self.masked_data[self.mask ==False]=np.nan
         #print('self.mask=',self.mask)
         self.data_masked_array= ma.array(self.data, mask=self.mask)
-<<<<<<< HEAD
  
-
         if directional_filtering==True:
        
             first_not_nan=np.where(np.isnan(self.masked_data[:,0])==False)[0][0]
@@ -814,86 +805,6 @@ class Storm(object):
             self.write_log('cutted & assigned data of oroginal shape' + str(data.shape))
             self.write_log('data cutted')
         
-        
-=======
-        #print('self.masked_data=',self.masked_data)
-        #print('self.data_masked_array=',self.data_masked_array)
-        
-        
-  ## Directional filtering:
-       
-        first_not_nan=np.where(np.isnan(self.masked_data[:,0])==False)[0][0]
-        print(first_not_nan)
-        #print(self.masked_data[:,0])
-        print(time_in['dt64'][np.where(time_in['sec']==self.time[first_not_nan])])
-        
-        last_not_nan=np.where(np.isnan(self.masked_data[:,-1])==False)[0][-1]
-        print(last_not_nan)
-        #print(self.masked_data[:,-1])
-        print(time_in['dt64'][np.where(time_in['sec']==self.time[last_not_nan])])
-        
-        time_length=last_not_nan-first_not_nan+1
-        print('time_length=',time_length)
-        
-        max_index=np.where(self.masked_data == np.nanmax(self.masked_data))
-        print(self.masked_data[max_index])
-        print('max_index=', max_index)
-        time_index_masked=max_index[0][0]
-        freq_index_masked=max_index[1][0]
-        time_index=np.where(time_in['sec']==self.time[time_index_masked])
-        freq_index=np.where(f_data==self.f[freq_index_masked])
-        print('time_index=',time_index, 'freq_index_=',freq_index)
-        
-        
-        max_index=np.where(self.masked_data == np.nanmax(self.masked_data[first_not_nan+1/5*time_length:last_not_nan-1/5*time_length,len(self.f)/4:3*len(self.f)/4]))
-        #print('max_index1=', max_index1)
-       # print(self.masked_data[max_index])
-        print('max_index=', max_index)
-        time_index_masked=max_index[0][0]
-        freq_index_masked=max_index[1][0]
-        time_index=np.where(time_in['sec']==self.time[time_index_masked])
-        freq_index=np.where(f_data==self.f[freq_index_masked])
-        print('time_index=',time_index, 'freq_index_=',freq_index)
-       
-        peak_direction=direction[time_index,freq_index]
-        print('peak_direction=', peak_direction)
-        
-        t_initial=self.time[0]
-        print('t_initial=', t_initial)
-        t_initial_index=np.where(time_in['sec']==t_initial)[0][0]
-        print('t_initial=', time_in['dt64'][t_initial_index])
-        
-        f_initial=self.f[0]
-        print('f_initial=', f_initial)
-        f_initial_index=np.where(f_data==f_initial)[0][0]
-        
-        if peak_direction<80:
-            for i in range(len(self.masked_data[:,1])):
-                for j in range(len(self.masked_data[1,:])):
-                    if peak_direction+80<direction[t_initial_index+i,f_initial_index+j]<360+peak_direction-80:
-                        self.masked_data[i,j]=np.nan
-        if peak_direction>280:
-            for i in range(len(self.masked_data[:,1])):
-                for j in range(len(self.masked_data[1,:])):
-                    if peak_direction-80>direction[t_initial_index+i,f_initial_index+j]>(peak_direction+80)-360:
-                        self.masked_data[i,j]=np.nan
-        if 80<peak_direction<280:
-            for i in range(len(self.masked_data[:,1])):
-                for j in range(len(self.masked_data[1,:])):
-                    if peak_direction+80<direction[t_initial_index+i,f_initial_index+j] or peak_direction-80>direction[t_initial_index+i,j]:
-                        self.masked_data[i,j]=np.nan                
-        #print(self.masked_data)          
-        
-        
-  
-        self.write_log('cutted & assigned data of oroginal shape' + str(data.shape))
-        self.write_log('data cutted')
-           
-                 
-        
-        
-
->>>>>>> 8ef2f2625ab3c1fbf467d06cf002aba67f6f8566
     def load(self, path, verbose=False):
         #load data and attibutes
         D= MT.pickle_load(self.ID,path, verbose)
